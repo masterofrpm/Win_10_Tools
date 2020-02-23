f5897cd20960bdd7f7f13ca5bab23eddc9fe92ef
Paul Meyers <likelyjunk@masterofrpm.com>
2020-02-22 23:47:21 -0600

    Create Choco_Installer.ps1

---
+++ Choco_Installer.ps1
@@ -0,0 +1,13 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; `
+  iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+$MySite = 'https://GitHub.com/masterofrpm/Win_10_Tools'
+$URL_Base = $MySite.Replace('GitHub','raw.GitHub')+'/master/'
+$List_Url = $URL_Base + 'choco_shortlist.csv'
+
+$installlist = Invoke-WebRequest $List_Url | ConvertFrom-Csv
+Write-Host $installlist
+Pause
+foreach ($Pkg in $installlist) {
+	choco install $Pkg --limit-output -y  --no-reduce-package-size
+	}
